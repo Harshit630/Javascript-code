7cc1e05987e93845f4d9d96c5f3bde485481d189
Harshit Pal <harshitpal63066@gmail.com>
2024-10-28 07:06:45 +0000

    conversion

---
+++ basics/03_Conversionandoperation.js
@@ -0,0 +1,24 @@
+let score = "33abr" //"" = string , output = string ..............if we write 33abc(null output =0, undefined output= NaN
+// true output = 1 ,"harshit" output = NaN
+ 
+//console.log (typeof score);
+//console.log(typeof (score)); // as a method
+
+let valueInNumber = Number (score) // change the string into number
+//console.log(typeof valueInNumber); // output = number
+//console.log(valueInNumber) // 33sbr - output = NaN (not a number)
+
+
+let isLoggedIn = 1 
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+/* "" = false
+"harsh" = true
+"1" = true */
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber) ;
+console.log(typeof stringNumber);
